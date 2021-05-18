19d9f4cff35aa12d32459767121253469e2974c4
Lionel <lalm84@live.com>
2021-05-17 22:31:25 -0300

    Update

---
+++ index.js
@@ -0,0 +1,25 @@
+        let usuario = "Usuario";
+        let contraseña = "12345";
+
+        let ingreseUsuario = prompt("Por favor ingrese su usuario.");
+        let ingresePass = prompt("Por favor ingrese su contraseña.");
+        
+        for (let i = 0; i < 3; i++) {
+                if ((ingreseUsuario == usuario) && (ingresePass == contraseña)){
+                        alert("Bienvenido Usuario");
+                        break;
+                }else{
+                        alert("Error: Usuario o contraseña incorrectos");
+                        
+                }
+                if (i != 2) {
+                ingreseUsuario = prompt("Por favor ingrese su usuario.");
+                ingresePass = prompt("Por favor ingrese su contraseña.");
+                }
+        }
+
+        /*while ((ingreseUsuario != usuario) && (ingresePass != contraseña)){
+                ingreseUsuario = prompt("Por favor ingrese su usuario.");
+                ingresePass = prompt("Por favor ingrese su contraseña.");
+        }
+        alert("Bienvenido Usuario");*/
