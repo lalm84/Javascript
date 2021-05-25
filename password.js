fd4b53bd42ef420324a3f6c8c775749419c1e126
Lionel <lalm84@live.com>
2021-05-25 20:12:42 -0300

    Update C5

--- password.js
+++ password.js
@@ -36,3 +36,16 @@ function solicitarMail(){
 solicitarUsuario();
 solicitarPass();
 solicitarMail();
+
+///////////////////////////////////////////////////////////////////
+
+var valornota1 = prompt("Ingrese 1er valor");
+var valornota2 = prompt("Ingrese 2do valor");
+var valornota3 = prompt("Ingrese 3er valor");
+
+function promedio (nota1, nota2, nota3) {
+        return (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) /3;        
+}
+alert("El promedio es:"+promedio(valornota1, valornota2, valornota3))
+
+/////////////////////////////////////////////////////////////////////
