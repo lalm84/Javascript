f218b55f2e6c0bfe9c7c3bf1feb0b097ed52b905
Lionel <lalm84@live.com>
2021-06-10 21:50:56 -0300

    Updating folder

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-var ejemplo_vModel = new Vue({
-    el: '#calcStock',
-    data: {      
-      frutas: [        
-        { nombre: "Naranja", cantidad: 10 },
-        { nombre: "Banana", cantidad: 12 },
-        { nombre: "Pera", cantidad: 5 }
-      ],      
-    },    
-  })
+let data =
+[ 
+    {
+    name:"Mustang"    
+    },
+    {
+    name:"ChevySS"
+    },
+    {
+    name:"Polara"
+    },
+    {
+    name:"Charger"
+    },
+    {
+    name:"Shelby Cobra"
+    }    
+];
+let list = document.getElementById("list");  
+data.forEach((item) => {
+let li = document.createElement("li");
+li.innerText = item.name;
+list.appendChild(li);
+});
 
-const Autos = ["Mustang", "ChevySS", "Polara", "Charger", "ShelbyCobra"];
+function listado(list){
+    var text = "";
+    var inputs = document.querySelectorAll("input[type=text]");
+    for (var i = 0; i < inputs.length; i++) {
+        text += inputs[i].value;
+    }
+    var li = document.createElement("li");
+    var node = document.createTextNode(text);
+    li.appendChild(node);
+    document.getElementById("list").appendChild(li);
+    data.push( {name:text} );
+    alert("Se agregó elemento " + text);
+    /* let boton = document.getElementById("btnAdd")
+    boton.addEventListener("click", onClick)
+    function onClick(){
+    data.push( {name:text} );
+    alert("Se agregó " + text + " a la lista");
+    } */
+}
 
-Autos.push('Camaro');
-console.log(Autos);
-
-alert(Autos);
